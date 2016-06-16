1b2fc43f7474ceddf58fbdbf2764fd90944f1185
AbhijitParate <abhijitpparate@gmail.com>
2016-06-16 01:40:22 -0700

    Implemented list of files and folders

---
+++ app/src/main/java/com/vmr/vmrdemo/FileFolderListAdapter.java
@@ -0,0 +1,84 @@
+package com.vmr.vmrdemo;/*
+ * Created by abhijit on 6/15/16.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Date;
+import java.util.List;
+
+public class FileFolderListAdapter extends ArrayAdapter<File> {
+
+    private Context context;
+    private int viewResourceId;
+    private List<File>items;
+
+    public FileFolderListAdapter(Context context, List<File> fileFoldersList) {
+        super(context, R.layout.file_folder_layout , fileFoldersList);
+        this.context = context;
+        this.viewResourceId = R.layout.file_folder_layout;
+        this.items = fileFoldersList;
+    }
+
+    public File getItem(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view ;
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(viewResourceId, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        /* create a new view of my layout and inflate it in the row */
+        final File fileFolder = items.get(position);
+
+        if (fileFolder != null) {
+            TextView fileName = (TextView) view.findViewById(R.id.tvFileName);
+            TextView fileSize = (TextView) view.findViewById(R.id.tvFileSize);
+            TextView fileTimestamp = (TextView) view.findViewById(R.id.tvTimeStamp);
+
+            if(fileName!=null) {
+                fileName.setText(fileFolder.getName());
+            }
+
+            if(fileSize!=null) {
+                if (fileFolder.isDirectory()) {
+                    File[] files = fileFolder.listFiles();
+                    int numOfFiles = 0;
+                    if (files != null) {
+                        numOfFiles = files.length;
+                    }
+                    String temp = numOfFiles + " Items";
+                    fileSize.setText(temp);
+                } else {
+                    String value = null;
+                    long size = fileFolder.length() / 1024;
+                    if (size >= 1024)
+                        value = size / 1024 + " Mb";
+                    else
+                        value = size + " Kb";
+                    fileSize.setText(value);
+                }
+            }
+
+            if(fileTimestamp!=null) {
+                Date lastModified = new Date(fileFolder.lastModified());
+                fileTimestamp.setText(lastModified.toString());
+            }
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/vmr/vmrdemo/HomeFragments/FragmentMyRecords.java
+++ app/src/main/java/com/vmr/vmrdemo/HomeFragments/FragmentMyRecords.java
@@ -3,24 +3,64 @@ package com.vmr.vmrdemo.HomeFragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.vmr.vmrdemo.FileFolderListAdapter;
 import com.vmr.vmrdemo.R;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FragmentMyRecords extends Fragment {
+
+    private FileFolderListAdapter mAdapter;
+    private List<File> mFileList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_records, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_records, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.lvMyRecords);
+
+        mFileList = getFileList();
+        mAdapter = new FileFolderListAdapter(getActivity(), mFileList);
+
+        listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = mFileList.get(i).getName();
+                Toast.makeText(getActivity(), text + " clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    private List<File> getFileList(){
+        File f = new File(Environment.getRootDirectory().getPath());
+        ArrayList<File> inFiles = new ArrayList<>();
+        File[] files = f.listFiles();
+        Collections.addAll(inFiles, files);
+        return inFiles;
+    }
+
 }
